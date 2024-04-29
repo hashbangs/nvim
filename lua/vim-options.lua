c26384c3731a490a3bd2f2ead32a3c6e8f7cee83
Solara <solara.at@proton.me>
2024-04-29 16:49:31 +0200

    added a oneline comment

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,6 +4,6 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set relativenumber")
-vim.g.mapleader = " "
+vim.g.mapleader = " " -- leader = space
 
 vim.keymap.set('n', '<leader>l', ':Lazy<CR>', {})
