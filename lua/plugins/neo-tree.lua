1f82965aed5ade8ad8a9b37ee2f3908397324c18
Solara <solara.at@proton.me>
2024-05-22 12:29:56 +0200

    buffer/git status

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,6 +8,13 @@ return {
     },
     config = function()
       vim.keymap.set('n', '<leader>n', ':Neotree filesystem toggle left<CR>', {})
+    
+    require("neo-tree").setup({
+      enable_git_status = false,
+      window = {
+        position = "left",
+        width = 25,
+      },
+    })
     end
-  }
-
+}
