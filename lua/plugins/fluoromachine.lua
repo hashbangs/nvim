16218acf611be80b5349e7b1d28fcdcef7b108d9
Solara <solara.at@proton.me>
2024-05-22 14:35:13 +0200

    LineNr color override

--- lua/plugins/fluoromachine.lua
+++ lua/plugins/fluoromachine.lua
@@ -1,13 +1,15 @@
 return {
   'maxmx03/fluoromachine.nvim',
-        config = function ()
-         local fm = require 'fluoromachine'
+  config = function ()
+    local fm = require 'fluoromachine'
 
-         fm.setup {
-            glow = true,
-            theme = 'retrowave'
-         }
+    fm.setup {
+      glow = true,
+      theme = 'retrowave'
+    }
 
-         vim.cmd.colorscheme 'fluoromachine'
-        end
+    vim.cmd.colorscheme 'fluoromachine'
+    -- colors override
+    vim.api.nvim_set_hl(0, 'LineNr', { fg = "#599eff"} )
+    end
 }
