4f74439cfaedb53ef4c0470ed3cbcaee3c22849a
Solara <solara.at@proton.me>
2024-05-22 12:32:14 +0200

    set hybrid line nums

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,7 +3,11 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+-- hybrid line numbers
 vim.cmd("set relativenumber")
+vim.cmd("set number")
+
+-- vim.cmd("set autochdir")
 vim.g.mapleader = " " -- leader = space
 
 vim.keymap.set('n', '<leader>l', ':Lazy<CR>', {})
