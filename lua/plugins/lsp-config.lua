96edc32a39e43e40b945715b2dd7ae7f56c3f913
Solara <solara.at@proton.me>
2025-10-15 19:05:12 +0200

    remove 'undefined global vim' lua warning

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -14,6 +14,7 @@ return {
     end
   },
   {
+    -- neovim 0.11
     "neovim/nvim-lspconfig",
     lazy = true,
     init = function()
@@ -21,5 +22,12 @@ return {
       vim.opt.runtimepath:prepend(lspConfigPath)
       vim.lsp.enable({"bashls", "clangd", "cssls", "html", "hyprls", "jsonls", "ltex", "lua_ls", "remark_ls", "texlab"})
     end,
+
+    -- remove the 'undefined global vim' warning
+    vim.lsp.config("lua_ls", {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { "vim" }}}}})
   }
 }
